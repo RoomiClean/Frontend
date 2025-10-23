2c337a053cf25b352cb25427258d487154bcf557
daeunleeeee <lde7953@gmail.com>
2025-10-23 18:54:25 +0900

    feat: 헤더 디자인 수정, 햄버거 버튼 추가

--- src/app/_components/atoms/Header.tsx
+++ src/app/_components/atoms/Header.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Logo from '@/assets/svg/RowLogo.svg';
+import Menu from '@/assets/svg/HamburgerMenu.svg';
 import Link from 'next/link';
 
 // interface HeaderProps {
@@ -7,13 +10,19 @@ import Link from 'next/link';
 // }
 
 export default function Header() {
+  const handleMenuClick = () => {
+    console.log('햄버거 메뉴 아이콘 클릭');
+  };
   return (
-    <div className="h-[68px] px-[25px] py-[12px] flex justify-between items-center border-b">
+    <div className="h-[68px] px-[36px] py-[12px] flex justify-between items-center border-b">
       <Image src={Logo} alt="Logo" />
       <div className="flex items-center gap-8">
         <Link href="/service-structure">서비스 제공 범위 및 구조</Link>
         <Link href="/how-to-use">서비스 이용 방법</Link>
         <Link href="/login">로그인</Link>
+        <div className="cursor-pointer" onClick={handleMenuClick}>
+          <Image src={Menu} alt="MenuIcon" />
+        </div>
       </div>
     </div>
   );
