c43e15e8bc06ab090086aa3b787c103516d20c0b
daeunleeeee <lde7953@gmail.com>
2025-11-08 03:11:58 +0900

    feat: API instance에 camelCase/snake_case 자동 변환 추가 (#27)

--- src/app/_lib/intance.ts
+++ src/app/_lib/intance.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import camelcaseKeys from 'camelcase-keys';
+import snakecaseKeys from 'snakecase-keys';
 
 const apiInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -6,6 +8,20 @@ const apiInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+apiInstance.interceptors.request.use(config => {
+  if (config.data) {
+    config.data = snakecaseKeys(config.data, { deep: true });
+  }
+  return config;
+});
+
+apiInstance.interceptors.response.use(response => {
+  if (response.data) {
+    response.data = camelcaseKeys(response.data, { deep: true });
+  }
+  return response;
 });
 
 export default apiInstance;
