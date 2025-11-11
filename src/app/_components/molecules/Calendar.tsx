85faedd129b8ba8bc5f1b35bc32daeb17680e012
daeunleeeee <lde7953@gmail.com>
2025-11-11 14:13:11 +0900

    feat: 캘린더 하단의 메모 영역 구현 (#22)

--- src/app/_components/molecules/Calendar.tsx
+++ src/app/_components/molecules/Calendar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-import { DisplaySmall, TitleSmall, BodyDefault } from '../atoms/Typography';
+import { DisplaySmall, TitleSmall, BodyDefault, TitleDefault } from '../atoms/Typography';
 import ArrowLeft from '@/assets/svg/ArrowLeft.svg';
 import ArrowRight from '@/assets/svg/ArrowRight.svg';
+import BulletPointIcon from '@/assets/svg/Bullet.svg';
 import { useCalendar } from '@/hooks/useCalendar';
 import { useCalendarMarkers } from '@/hooks/useCalendarMarkers';
 
@@ -32,8 +33,11 @@ interface CalendarProps {
   lastCleaningSchedule?: CleaningSchedule;
   /** 예약된 청소 일정 배열 */
   reservedCleaningSchedules?: CleaningSchedule[];
+  /** 캘린더 하단에 표시할 메모 목록 */
+  memos?: string[];
   initialYear?: number;
   initialMonth?: number;
+  className?: string;
 }
 
 /**
@@ -70,8 +74,10 @@ export const Calendar = ({
   accommodationSchedules = [],
   lastCleaningSchedule,
   reservedCleaningSchedules = [],
+  memos = [],
   initialYear,
   initialMonth,
+  className = '',
 }: CalendarProps) => {
   const { currentYear, currentMonth, dates, handlePrevMonth, handleNextMonth } = useCalendar({
     initialYear,
@@ -89,7 +95,9 @@ export const Calendar = ({
   const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
 
   return (
-    <div className="w-full h-full bg-neutral-100 border border-neutral-300 rounded-[20px] shadow-[0_6px_15px_0_rgba(0,0,0,0.1)] p-6 flex flex-col">
+    <div
+      className={`w-full h-full bg-neutral-100 border border-neutral-300 rounded-[20px] shadow-[0_6px_15px_0_rgba(0,0,0,0.1)] p-6 flex flex-col  overflow-visible ${className}`}
+    >
       <div className="flex items-center justify-center gap-[57px] mb-4">
         <button
           onClick={handlePrevMonth}
@@ -147,17 +155,17 @@ export const Calendar = ({
 
               {/* 마지막 청소 일정 마커 */}
               {markers.isLastCleaning && !markers.isAccommodation && (
-                <div className="absolute w-[2.6em] h-[2.6em] bg-green-alpha30 border border-green-alpha50 rounded-full" />
+                <div className="absolute w-[2em] h-[2em] bg-green-alpha30 border border-green-alpha50 rounded-full" />
               )}
 
               {/* 예약된 청소 일정 마커 */}
               {markers.isReservedCleaning && !markers.isAccommodation && (
-                <div className="absolute w-[2.6em] h-[2.6em] bg-red-alpha30 border border-red-alpha50 rounded-full" />
+                <div className="absolute w-[2em] h-[2em] bg-red-alpha30 border border-red-alpha50 rounded-full" />
               )}
 
               {/* 오늘 날짜 테두리 */}
               {markers.isToday && (
-                <div className="absolute w-[2.6em] h-[2.6em] border-[1px] border-red-100 rounded-full" />
+                <div className="absolute w-[2em] h-[2em] border-[1px] border-red-100 rounded-full" />
               )}
 
               {/* 날짜 텍스트 */}
@@ -166,6 +174,20 @@ export const Calendar = ({
           );
         })}
       </div>
+
+      {memos && memos.length > 0 && (
+        <>
+          <div className="border-t border-neutral-300 my-4" />
+          <div className="space-y-2">
+            {memos.map((memo, index) => (
+              <div key={index} className="flex items-start gap-2">
+                <Image src={BulletPointIcon} alt="bullet-point" className="mt-[3px]" />
+                <TitleDefault className="text-neutral-1000 flex-1">{memo}</TitleDefault>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
